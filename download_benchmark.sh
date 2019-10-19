285e0b5efff67792a52ec50ba1cc8b8c7c561401
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2019-10-19 18:45:20 +0200

    Github CI complains

--- download_benchmark.sh
+++ download_benchmark.sh
@@ -1,5 +1,5 @@
-mkdir ~/readability-metric/db
-mkdir ~/readability-metric/test/books
+mkdir db
+mkdir test/books
 wget https://www.gutenberg.org/ebooks/24264.epub.noimages?session_id=13a48cb17a2a788bd0df32eb9d11b2cc90e5ffb6 -O test/books/hongloumeng.epub
 wget https://www.gutenberg.org/ebooks/6099.epub.noimages?session_id=e525c6c0f4f2faf96f365aabedf179ef08f4f236 -O test/books/lesfleursdumal.epub
 wget https://www.gutenberg.org/ebooks/21000.epub.noimages?session_id=e525c6c0f4f2faf96f365aabedf179ef08f4f236 -O test/books/faust.epub
