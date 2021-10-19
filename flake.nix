19cbabf15f21351fd64ff5380844004646e3649d
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-10-19 20:14:01 +0200

    WIP

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "My Python application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        customOverrides = self: super: {
+          # Overrides go here
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides =
+            [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+        };
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        packageName = "poetry test";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage =
+          # Notice the reference to nixpkgs here.
+          with import nixpkgs { system = "x86_64-linux"; };
+          stdenv.mkDerivation {
+            name = "quicksort";
+            src = self;
+            dontStrip = true;
+            buildPhase = "gcc -O0 -g -o quicksort ./quicksort.cpp -lstdc++";
+            installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
+          };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ gdb rr poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
